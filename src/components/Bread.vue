5e1ecd98f839dc8c543dfeb4e832676ae362ec54
Administrator <971302145@qq.com>
2018-02-27 02:05:55 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	server/views/index.html

--- src/components/Bread.vue
+++ src/components/Bread.vue
@@ -4,7 +4,7 @@
             <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item>活动管理</el-breadcrumb-item>
             <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详</el-breadcrumb-item>
         </el-breadcrumb>
     </div>
 </template>
